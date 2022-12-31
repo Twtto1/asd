ec1fcd20d05b6ce067c75cdb7e8c5675d0ebd46c
CP <test@gmail.com>
2022-12-31 22:51:25 +0200

    novCommit

--- varpass.js
+++ varpass.js
@@ -2,13 +2,13 @@ var passwords = new Object;
 passwords['qwe'] = 'qwe';
 
 function check_password(tries){
-    var username = prompt('Please enter your username');
-    var password = prompt('Please enter your password');
+    var username = prompt('Dve imena');
+    var password = prompt('Parola');
     if(passwords[username] != password){
         if(tries == 3){
             window.location = 'http://www.google.com/';
         }else{
-            check_password(tries + 1);
+            check_password(tries + 2);
         }
     }
 }
